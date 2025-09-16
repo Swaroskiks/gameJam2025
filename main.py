280dfc2e8d2a61cc1481468eb293044a2bb24259
Mouad Moustarzak <Mouad.moustarzak@etu.unice.fr>
2025-09-16 11:24:06 +0200

    Ajout background

--- main.py
+++ main.py
@@ -4,19 +4,23 @@ from src.settings import WIDTH, HEIGHT, FPS, BLACK, WHITE
 
 def main():
     pygame.init()
-    pygame.display.set_caption("GameJam 2025")
+    pygame.display.set_caption("The Horse")
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     clock = pygame.time.Clock()
-    running = True
+    
+    background = pygame.image.load("assets/images/background.png").convert()
+    background = pygame.transform.scale(background, (WIDTH, HEIGHT))
 
+    running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-        screen.fill(BLACK)
+        screen.blit(background, (0, 0))
         pygame.display.flip()
         clock.tick(FPS)
+
 
     pygame.quit()
     sys.exit()
